c1ff0c18584fdded53f734f62b63de60ee377308
kongdengyuan <kongdengyuan@gmail.com>
2017-06-02 10:19:49 +0800

    Adjust format

--- sap/migration/mirror/eu_import_schema.sh
+++ sap/migration/mirror/eu_import_schema.sh
@@ -12,7 +12,8 @@ $HDBSQL_COMMAND "\ds" | grep $DB_SCHEMA &>/dev/null
 
 if [ $? -eq 0 ];then 
     echo -e "\e[1;31mSchema already imported\e[0m"
-$HDBSQL_COMMAND "\ds" | grep $DB_SCHEMA | cut -d',' -f1  | sed 's/"//g'| awk '{print "drop schema "$1" cascade"}' | $HDBSQL_COMMAND &>/dev/null &&echo -e "\e[1;31mDrop schema success\e[0m"  && $HDBSQL_COMMAND "IMPORT ${DB_SCHEMA}.\"*\" AS BINARY FROM '${IMPORT_DIR}/${T_DIR}' with threads 20" &>/dev/null && rm ${T_DIR}.tgz && sudo rm -r ${IMPORT_DIR}/${T_DIR}&& echo -e "\e[1;32mImport schema Success \e[0m"
+$HDBSQL_COMMAND "\ds" | grep $DB_SCHEMA | cut -d',' -f1  | sed 's/"//g'| awk '{print "drop schema "$1" cascade"}' | $HDBSQL_COMMAND &>/dev/null && echo -e "\e[1;31mDrop schema success\e[0m"  && 
+$HDBSQL_COMMAND "IMPORT ${DB_SCHEMA}.\"*\" AS BINARY FROM '${IMPORT_DIR}/${T_DIR}' with threads 20" &>/dev/null && rm ${T_DIR}.tgz && sudo rm -r ${IMPORT_DIR}/${T_DIR}&& echo -e "\e[1;32mImport schema Success \e[0m"
  else
 $HDBSQL_COMMAND "IMPORT ${DB_SCHEMA}.\"*\" AS BINARY FROM '${IMPORT_DIR}/${T_DIR}' with threads 20" &>/dev/null && rm ${T_DIR}.tgz  &&  sudo rm -r ${IMPORT_DIR}/${T_DIR} &&  echo -e "\e[1;32mImport schema Success \e[0m"
 fi
