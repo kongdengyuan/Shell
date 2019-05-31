4cd85ec0642b5e68f08c4f38320c9141c63fbe9b
kongdengyuan <kongdengyuan@gmail.com>
2019-05-31 09:35:18 +0800

    Update useradd.sh

--- useradd.sh
+++ useradd.sh
@@ -9,3 +9,5 @@ echo -e "\e[1;31m$USER already exists\e[0m"
 else
    groupadd $USER && useradd -m $USER -d /home/$USER -g $USER -s /bin/bash &&   echo -e "\e[1;32mCreate user Success ! \e[0m"
 fi
+
+#  ssh user@host 'mkdir -p .ssh && cat >> .ssh/authorized_keys' < ~/.ssh/id_rsa.pub
